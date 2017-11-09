e711dcbcb618f54c4de271c86caf6b1388663082
Leather Dog Muksihs <muksihs@gmail.com>
2017-11-08 20:58:52 -0500

    IsSdm

--- src/main/java/muksihs/e621/resteemit/client/E621EP.java
+++ src/main/java/muksihs/e621/resteemit/client/E621EP.java
@@ -2,7 +2,7 @@ package muksihs.e621.resteemit.client;
 
 import com.google.gwt.core.client.EntryPoint;
 
-public class E621EP implements EntryPoint {
+public class E621EP implements EntryPoint, IsSdm {
 
 	@Override
 	public void onModuleLoad() {
