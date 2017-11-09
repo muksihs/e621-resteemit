2b74bc25900dc5134aa38b608624d11651310492
Leather Dog Muksihs <muksihs@gmail.com>
2017-11-08 19:17:33 -0500

    initial entry point setup

---
+++ src/main/java/muksihs/e621/resteemit/E621EP.java
@@ -0,0 +1,12 @@
+package muksihs.e621.resteemit;
+
+import com.google.gwt.core.client.EntryPoint;
+
+public class E621EP implements EntryPoint {
+
+	@Override
+	public void onModuleLoad() {
+
+	}
+
+}
