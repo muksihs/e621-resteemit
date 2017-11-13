78bf287ec9fe917f5ae92606dbd04b6059724f7f
Leather Dog Muksihs <muksihs@gmail.com>
2017-11-13 00:17:12 -0500

    version bump

--- src/main/java/muksihs/e621/resteemit/shared/Consts.java
+++ src/main/java/muksihs/e621/resteemit/shared/Consts.java
@@ -1,7 +1,7 @@
 package muksihs.e621.resteemit.shared;
 
 public class Consts {
-	public static final String APP_VERSION = "20171109";
+	public static final String APP_VERSION = "20171113";
 	public static final String E621_SHOW_POST = "https://e621.net/post/show/";
 	public static final int PREVIEWS_TO_SHOW = 6;
 
