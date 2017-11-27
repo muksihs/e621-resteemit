ed600e972a3e6eeba4bbb99cbe8fe6bc14c88130
Leather Dog Muksihs <muksihs@gmail.com>
2017-11-26 20:21:58 -0500

    autocommit on build

--- src/main/java/steem/CommentResult.java
+++ src/main/java/steem/CommentResult.java
@@ -4,5 +4,5 @@ import jsinterop.annotations.JsPackage;
 import jsinterop.annotations.JsType;
 
 @JsType(isNative = true, namespace = JsPackage.GLOBAL)
-public class CommentResult {
+public interface CommentResult {
 }
